5b4da1cbe93e7d694fbef4204621c6ccb8b7fcab
Liam <liam.masters3@gmail.com>
2020-08-19 18:22:22 -0400

    Add work timeline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, {useState} from 'react';
-import gh from './gh.svg';
-import li from './li.svg';
+import React from 'react';
 import './App.css';
 import TextLoop from "react-text-loop";
+import {Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineOppositeContent, TimelineDot} from "@material-ui/lab";
+import Paper from '@material-ui/core/Paper';
+import Typography from "@material-ui/core/Typography";
+
+import gh from './gh.svg';
+import li from './li.svg';
+import uw from './uw.svg';
+import ss from './ss.png';
+import ec from './ec.jpg';
+import ntc from './ntc.ico';
+import volt from './volt.png';
 
 function scrollToDiv(divRef) {
   divRef.current.scrollIntoView({behavior: "smooth", block: "start"})
@@ -70,6 +79,154 @@ class App extends React.Component {
         </div>
         <div>
           <div className="section-header" ref={sections[0]}>Professional</div>
+            <Timeline align="left">
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography variant="body2" className="timeframe">
+                    Sep 2016 - Dec 2019
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot>
+                    <img className="timeline-icon" src={uw}/>
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="timeline-item">
+                  <Paper elevation={3} className="paper">
+                    <Typography variant="h6" component="h1">
+                      Began BSc Physics and Astronomy
+                    </Typography>
+                    <Typography>University of Waterloo</Typography>
+                  </Paper>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography variant="body2" className="timeframe">
+                    Sep 2017 - Dec 2019
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot>
+                  <img className="timeline-icon" src={ss}/>
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="timeline-item">
+                  <Paper elevation={3} className="paper">
+                    <Typography variant="h6" component="h1">
+                      Media Coordinator
+                    </Typography>
+                    <Typography>UW Science Society</Typography>
+                  </Paper>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography variant="body2" className="timeframe">
+                    Jan 2018 - Apr 2018
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot>
+                  <img className="timeline-icon round" src={ec}/>
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="timeline-item">
+                  <Paper elevation={3} className="paper">
+                    <Typography variant="h6" component="h1">
+                      Modelling Technician
+                    </Typography>
+                    <Typography>Environment and Climate Change Canada</Typography>
+                  </Paper>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography variant="body2" className="timeframe">
+                    Sep 2018 - Apr 2019
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot>
+                    <img className="timeline-icon round" src={ntc}/>
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="timeline-item">
+                  <Paper elevation={3} className="paper">
+                    <Typography variant="h6" component="h1">
+                      Software Co-op
+                    </Typography>
+                    <Typography>Neptec Technologies</Typography>
+                  </Paper>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography variant="body2" className="timeframe">
+                    Sep 2019 - Dec 2019
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot>
+                    <img className="timeline-icon" src={volt}/>
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="timeline-item">
+                  <Paper elevation={3} className="paper">
+                    <Typography variant="h6" component="h1">
+                      Software Intern
+                    </Typography>
+                    <Typography>Volta Labs, Inc.</Typography>
+                  </Paper>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography variant="body2" className="timeframe">
+                    Apr 2020
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot>
+                    <img className="timeline-icon" src={uw}/>
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="timeline-item">
+                  <Paper elevation={3} className="paper">
+                    <Typography variant="h6" component="h1">
+                      Graduated BSc General Science, Astrophysics Minor
+                    </Typography>
+                    <Typography>University of Waterloo</Typography>
+                  </Paper>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography variant="body2" className="timeframe">
+                    Jan 2019 - Present
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot>
+                    <img className="timeline-icon" src={volt}/>
+                  </TimelineDot>
+                </TimelineSeparator>
+                <TimelineContent className="timeline-item">
+                  <Paper elevation={3} className="paper">
+                    <Typography variant="h6" component="h1">
+                      Software Engineer
+                    </Typography>
+                    <Typography>Volta Labs, Inc.</Typography>
+                  </Paper>
+                </TimelineContent>
+              </TimelineItem>
+            </Timeline>
           <div className="section-header" ref={sections[1]}>Projects</div>
           <div className="section-header" ref={sections[2]}>Music</div>
           <iframe 
