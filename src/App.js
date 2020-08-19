d738170476a4a20bdd1e34ce8426cff0c4dbbf02
Liam <liam.masters3@gmail.com>
2020-08-19 12:31:40 -0400

    Add homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextLoop from "react-text-loop";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="header">Liam Masters</div>
+      <TextLoop 
+        interval={2500}
+        springConfig={{stiffness: 250, damping: 15}}
+      >
+        <div id="subheader">Software Engineer</div>
+        <div id="subheader">Maker</div>
+        <div id="subheader">Musician</div>
+        <div id="subheader">Podcast Aficianado</div>
+      </TextLoop>
     </div>
   );
 }
