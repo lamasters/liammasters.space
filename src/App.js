0d0dade72a97b807945fc73ed8435d4fbf2de704
lamasters <liam.masters3@gmail.com>
2020-08-21 09:29:06 -0400

    Update star parameters

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
     if (window.innerWidth > 800) {
       playerWidth = "800";
       showNav = true;
-      numStars = 500;
+      numStars = 300;
     }
 
     let initStarfield = this.initStarfield.bind(this);
@@ -69,7 +69,7 @@ class App extends React.Component {
       y = Math.floor(Math.random() * height);
       vx = ((1 + Math.random()) * dirs[Math.floor(Math.random() * dirs.length)]) / 4;
       vy = ((1 + Math.random()) * dirs[Math.floor(Math.random() * dirs.length)]) / 4;
-      size = Math.floor(1 + Math.random() * 3);
+      size = Math.floor(2 + Math.random() * 5);
       opac = Math.random();
       stars.push({
         x: x,
@@ -101,7 +101,7 @@ class App extends React.Component {
         y = Math.floor(Math.random() * height);
         vx = ((1 + Math.random()) * dirs[Math.floor(Math.random() * dirs.length)]) / 4;
         vy = ((1 + Math.random()) * dirs[Math.floor(Math.random() * dirs.length)]) / 4;
-        size = Math.floor(1 + Math.random() * 3);
+        size = Math.floor(2 + Math.random() * 5);
         opac = Math.random();
         stars.push({
           x: x,
